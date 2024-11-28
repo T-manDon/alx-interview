22794fa02d2dc37ed2789c7d4bf82c87762603d6
T-manDon <opiyoadiema@gmail.com>
2024-11-28 20:20:54 +0000

    all

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This function makes the change
+"""
+
+
+def makeChange(coins, total):
+    """
+    This func gives min num of coins to give a certain total
+    Args:
+        coins (list of ints): lists distict coin values
+        total (int): total target value
+    Return:
+        Num of coins if threshold not met
+    """
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
